57ed2b044461a660b1973fdf30f15402a7aeefc6
hakirot <hackirot@proton.me>
2022-06-19 18:08:13 -0700

    Graphs - Init, Add, Search, Print

--- src/graph.c
+++ src/graph.c
@@ -26,8 +26,13 @@
  *  multiple flights between cities)
  *
  *  We can represent links in a graph with adjacency lists or adjacency matrix
- *   - Matrix (used prevously) uses O(v^2) of space
- *
+ *   - Matrix (used prevously) uses O(v^2) of space because we store NO CONNECTIONS as well (redundant)
+ *   - Most of real-world graphs are sparse, in that vertices are connected to few other vertices
+ *      - May want to use an array of linked lists (buckets)
+ *          - Space complexity of this method is O(e)
+ *          - However, time complexity for finding if two nodes are connected is, worst case, O(v)
+ *              - Binary search would be log(v), but keeping an array sorted is costly
+ *              - For an adjacency matrix, complexity would be O(1), since we would know the position of the connection
  *  Path (aka walk): a sequence of vertices where each adjacent pair is connected by an edge
  *  Simple Path: a path in which no vertices (and thus no edges) are repeated
  *  Trail: a walk in which no edges are repeated but vertices may be repeated
@@ -41,21 +46,147 @@
 
 #include <stdio.h>
 #include <stdlib.h>
-#include <string.h>
-#define MAX_SIZE 101
+#define NUM_VERTICES 8
 
-typedef struct Edge Edge;
+typedef struct Node Node;
 
-struct Edge {
-    int startVertex; // integers for vertex can represent positions of names in an array
-    int endVertex;
-    int weight;
+char Names[NUM_VERTICES] = {'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H'};
+
+struct Node {
+    int data; // represents the neighbor in the Array list
+    Node* next; // Next neighbor
+    //int weight; // Optional weight
 };
 
-char* name_list[MAX_SIZE];
-Edge edge_list[MAX_SIZE];
+void InitArray(Node** array);
+Node* GetNewNode(int data);
+void AddConnection(Node** array, char vertex, char neighbor);
+int SearchConnection(Node** array, char vertex, char neighbor);
+void PrintGraph(Node** array);
 
 int main(int argc, char * argv[]){
 
+    Node* Array[NUM_VERTICES];
+    InitArray(Array);
+
+    // Create relationships
+    AddConnection(Array, 'A', 'B');
+    AddConnection(Array, 'A', 'C');
+    //AddConnection(Array, 'A', 'C'); // Error occurs correctly
+    AddConnection(Array, 'A', 'D');
+    AddConnection(Array, 'A', 'F');
+    AddConnection(Array, 'B', 'C');
+    AddConnection(Array, 'F', 'H');
+    PrintGraph(Array);
+
     return 0;
 }
+
+Node* GetNewNode(int data){
+
+    Node* newNode = (Node*)malloc(sizeof(Node));
+    newNode->next = NULL;
+    newNode->data = data;
+    return newNode;
+}
+
+void AddConnection(Node** array, char vertex, char neighbor){
+
+    int a = vertex - 65;
+    int b = neighbor - 65;
+    if (a < 0 || a >= 0 + NUM_VERTICES ||
+        b < 0 || b >= 0 + NUM_VERTICES ||
+        a == b)
+    {
+        if(a == b){
+            printf("Error: Duplicate vertex\n");
+            return;
+        } else {
+            printf("Error: Vertex unknown\n");
+            return;
+        }
+    }
+
+    if(SearchConnection(array, vertex, neighbor)){
+        printf("Error: %c already connected to %c\n", vertex, neighbor);
+        return;
+    }
+
+
+    if(array[a] == NULL){
+        array[a] = GetNewNode(b);
+
+    } else {
+
+        Node* temp = array[a];
+
+        while(temp->next != NULL){
+            temp = temp->next;
+        }
+        temp->next = GetNewNode(b);
+    }
+
+    // (if A -> B, then B also has connected to A)
+    if(SearchConnection(array, neighbor, vertex)){
+        printf("Error: %c already connected to %c\n", vertex, neighbor);
+        return;
+    }
+    if(array[b] == NULL){
+        array[b] = GetNewNode(a);
+    } else {
+        Node* temp = array[b];
+        while(temp->next != NULL){
+            temp = temp->next;
+        }
+        temp->next = GetNewNode(a);
+    }
+
+    return;
+}
+
+int SearchConnection(Node** array, char vertex, char neighbor){
+
+    //if connection exists return 1
+    Node* temp = array[vertex - 65];
+
+    if (temp == NULL){
+        return 0;
+    } else {
+        while(temp != NULL){
+            if(temp->data == neighbor - 65){
+                return 1;
+            }
+            temp = temp->next;
+        }
+    }
+
+    return 0;
+}
+
+void InitArray(Node** array){
+
+    for(int i = 0; i < NUM_VERTICES; i++){
+        array[i] = NULL;
+    }
+}
+
+void PrintGraph(Node** array){
+    for(int i = 0; i < NUM_VERTICES; i++){
+        printf("%c: ", i + 65);
+        Node* temp = array[i];
+        while(temp != NULL){
+            printf("%c ", temp->data + 65);
+            temp = temp->next;
+        }
+        printf("\n");
+    }
+
+    return;
+}
+
+/*
+    if(vertex == 66){
+        printf("..paused\n");
+        getchar();
+    }
+*/
